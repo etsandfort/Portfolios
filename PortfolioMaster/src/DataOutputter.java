d00bab3d9201378afc31a79e6404fde693412778
Libby Gentry <libby_gentry@yahoo.com>
2015-02-19 21:54:19 -0600

    Simplifying main

--- PortfolioMaster/src/DataOutputter.java
+++ PortfolioMaster/src/DataOutputter.java
@@ -39,7 +39,7 @@ public class DataOutputter {
 	public void setFormat(DataFormatter format) {
 		this.format = format;
 	}
-
+	
 	/**
 	 * This is the main method, it runs the program
 	 * @param args the set of arguments provided at runtime
@@ -47,11 +47,6 @@ public class DataOutputter {
 	public static void main(String args[]){
 		DataOutputter dataOut = new DataOutputter();
 				
-		//Converting the input files to .json and .xml output files
-//		do.getFormat().outputJsonAssets("data/Assets.json", dc.getInput().readAssets());
-//		do.getFormat().outputXmlAssets("data/Assets.xml", dc.getInput().readAssets());
-//		do.getFormat().outputJsonPersons("data/Persons.json", dc.getInput().readPersons());
-//		do.getFormat().outputXmlPersons("data/Persons.xml", dc.getInput().readPersons());
 		dataOut.getFormat().printSummaryReport();
 		dataOut.getFormat().printDetailedReport();
 	}
