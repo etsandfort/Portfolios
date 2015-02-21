5242b088f18c9fe1fb574d11eb634e57433cbf45
Libby Gentry <libby_gentry@yahoo.com>
2015-02-20 21:50:24 -0600

    fixed portfolio parameters

--- PortfolioMaster/src/ReportMaker.java
+++ PortfolioMaster/src/ReportMaker.java
@@ -3,15 +3,17 @@ import java.util.ArrayList;
 public class ReportMaker {
 	
 	private ReportCalculation reportCalc;
+	private ArrayList<Portfolio> portfolios;
 	
 	public ReportMaker(ArrayList<Portfolio> portfolios) {
 		reportCalc = new ReportCalculation(portfolios);
+		this.portfolios = portfolios;
 	}
 	
 	/**
 	 * This method prints the Portfolio Summary Report.
 	 */
-	public void printSummaryReport(ArrayList<Portfolio> portfolios){
+	public void printSummaryReport(){
 		System.out.println("Portfolio Summary Report");
 		printThickLine();
 		
@@ -48,39 +50,16 @@ public class ReportMaker {
 	/**
 	 * This method prints the Detailed Report for each portfolio, in order alphabetically by Owner's Last Name
 	 */
-	public void printDetailedReport(ArrayList<Portfolio> portfolios){
+	public void printDetailedReport(){
 		System.out.println("Portfolio Details");
 		printThickLine();
 		
-		String nameOwner = "";
-		String nameManager = "";
- 		String nameBeneficiary = "none";
+
  		
  		// iterating through each portfolio
-		for(int i = 0; i < portfolios.size(); i++){
+		for(int i = 0; i < portfolios.size(); i++) {
 			
-			// set the name of the portfolio's owner
-			nameOwner = portfolios.get(i).getOwner().getLastName()
-					    + ", " + portfolios.get(i).getOwner().getFirstName();
-			
-			// set the name of the portfolio's manager
-			nameManager = portfolios.get(i).getManager().getLastName()
-						  + ", " + portfolios.get(i).getManager().getFirstName();
-			
-			// set the name of the portfolio's beneficiary, if there is one
-			if(portfolios.get(i).getBeneficiary() != null){
-				nameBeneficiary = portfolios.get(i).getBeneficiary().getLastName()
-								  + ", " + portfolios.get(i).getBeneficiary().getFirstName();
-			}
-			
-			System.out.println("Portfolio" + portfolios.get(i).getCode());
-			printThinLine(40);
-			
-			System.out.printf("%-15s %-25s\n", "Owner:", nameOwner);
-			System.out.printf("%-15s %-25s\n", "Manager:", nameManager);
-			System.out.printf("%-15s %-25s\n", "Beneficiary:", nameBeneficiary);
-			System.out.println("Assets");
-			System.out.printf("%-10s %-43s %20s %-5s %17s %15s\n", "Code", "Asset", "Return Rate", "Risk", "Annual Return", "Value");
+			setUpDetailedReport(i);
 			
 			int numAssets = portfolios.get(i).getAssets().size();
 			
@@ -93,19 +72,53 @@ public class ReportMaker {
 					"$", portfolios.get(i).getAssets().get(j).getAnnualReturn(), 
 					"$", portfolios.get(i).getAssets().get(j).getValue());
 			}
-			System.out.printf("%48s %-7s %-15d %-15d %20d\n", "", "Totals:", portfolios.get(i).getTotalRisks(), portfolios.get(i).getTotalAnnualReturns(), portfolios.get(i).getTotalValue());
+			
+			System.out.printf("%48s %-7s %-15d %-15d %20d\n", "", "Totals:", 
+					portfolios.get(i).getTotalRisks(), 
+					portfolios.get(i).getTotalAnnualReturns(), 
+					portfolios.get(i).getTotalValue());
 		}
+	}
+	
+	public void setUpDetailedReport(int portfolioNum) {
+		String nameOwner = "";
+		String nameManager = "";
+ 		String nameBeneficiary = "none";
+		
+		// set the name of the portfolio's owner
+		nameOwner = portfolios.get(portfolioNum).getOwner().getLastName()
+				    + ", " + portfolios.get(portfolioNum).getOwner().getFirstName();
+		
+		// set the name of the portfolio's manager
+		nameManager = portfolios.get(portfolioNum).getManager().getLastName()
+					  + ", " + portfolios.get(portfolioNum).getManager().getFirstName();
+		
+		// set the name of the portfolio's beneficiary, if there is one
+		if(portfolios.get(portfolioNum).getBeneficiary() != null) {
+			nameBeneficiary = portfolios.get(portfolioNum).getBeneficiary().getLastName()
+							  + ", " + portfolios.get(portfolioNum).getBeneficiary().getFirstName();
+		}
+		
+		System.out.println("Portfolio" + portfolios.get(portfolioNum).getCode());
+		printThinLine(40);
+		
+		System.out.printf("%-15s %-25s\n", "Owner:", nameOwner);
+		System.out.printf("%-15s %-25s\n", "Manager:", nameManager);
+		System.out.printf("%-15s %-25s\n", "Beneficiary:", nameBeneficiary);
+		System.out.println("Assets");
+		System.out.printf("%-10s %-43s %20s %-5s %17s %15s\n", "Code", "Asset", 
+						  "Return Rate", "Risk", "Annual Return", "Value");
 	}
 	
 	/**
 	 * Prints a thick line; used to better format portfolio reports
 	 */
-	public static void printThickLine(){
+	public static void printThickLine() {
 		for(int i = 0; i < 150; i++){
-			if(i != 149){
+			if(i != 149) {
 				System.out.print('=');				
 			}
-			else{
+			else {
 				System.out.println('=');
 			}
 		}
@@ -114,15 +127,15 @@ public class ReportMaker {
 	/**
 	 * Prints a hyphenated line; used to better format portfolio reports
 	 */
-	public static void printThinLine(int lineLength){
-		for(int i = 0; i < lineLength; i++){
-			if(i < lineLength - 90){ // print blank spaces because can't sum codes, owners or managers
+	public static void printThinLine(int lineLength) {
+		for(int i = 0; i < lineLength; i++) {
+			if(i < lineLength - 90) { // print blank spaces because can't sum codes, owners or managers
 				System.out.print(" ");
 			}
-			else if(i != (lineLength - 1)){
+			else if(i != (lineLength - 1)) {
 				System.out.print('-');				
 			}
-			else{
+			else { // last char to be printed, so start new line after printing
 				System.out.println('-');
 			}
 		}
